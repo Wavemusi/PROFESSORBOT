b3b7f816153f42546e0a5532ddcabb31ae5cd9d7
Wave <95126252+Wavemusi@users.noreply.github.com>
2023-10-12 19:57:49 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
